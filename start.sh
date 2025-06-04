ade636b8dde42c0b27966aa2dd6e31f370f07afb
𝕏 <63678199+pinches385@users.noreply.github.com>
2025-06-04 14:10:46 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Download V2Ray
+# Install V2Ray
 mkdir -p /usr/bin /etc/v2ray
-wget -O /tmp/v2ray-linux.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
-unzip /tmp/v2ray-linux.zip -d /tmp/v2ray
+wget -O /tmp/v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+unzip /tmp/v2ray.zip -d /tmp/v2ray
 install -m 755 /tmp/v2ray/v2ray /usr/bin/v2ray
 
-# Start Caddy
+# Run Caddy (reverse proxy) in background
 caddy run --config /etc/caddy/Caddyfile &
 
-# Start V2Ray with your config
-/usr/bin/v2ray run -config /app/config.json
+# Run V2Ray (main process)
+v2ray run -config /app/config.json
